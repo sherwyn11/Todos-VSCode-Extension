5d515819b69c8ea0a6e8f7ba91db868c043c6d43
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-04-12 17:53:02 +0530

    [dev] updated views for todos

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,7 +1,54 @@
+import { useState, useEffect } from 'react';
+
 export default function Sidebar() {
+
+  interface TODO {
+    text: string,
+    completed: boolean
+  }
+  let [text, setText] = useState("");
+  let [todos, setTodos] = useState<Array<TODO>>([]);
+  let [printTodos, setprintTodos] = useState<any>([]);
+
+  useEffect(() => {
+    printTodoElements();
+  }, [todos]);
+
+  const updateText = (e: any) => {
+    setText(e.target.value);
+  }
+
+  const addTodo = (e: any) => {
+    e.preventDefault();
+    setTodos([{ text: text, completed: false }, ...todos]);
+    setText('');
+  }
+
+  const updateStatus = (e: any) => {
+    console.log(e);
+    let selectedTodo: TODO = todos.filter((todo) => { return todo.text === e.target.innerHTML })[0];
+    console.log(selectedTodo)
+    selectedTodo.completed = !selectedTodo.completed;
+    printTodoElements();
+  }
+
+  function printTodoElements () {
+    console.log(todos);
+    let t = todos.map((todo) => {
+      console.log(todo);
+      return <ul><li value={todo.text} onClick={updateStatus} style={ todo.completed ? {textDecoration: "line-through"} : {} }>{todo.text}</li></ul>;
+    });
+    console.log(t);
+    setprintTodos(t);
+  }
+
   return (
     <div>
-      <h1>Hello from React Sidebar!</h1>  
-      </div>
-    );
+      <form>
+        <input onChange={updateText} value={text}/><br></br>
+        <button onClick={addTodo}>Add Todo</button>
+      </form>
+      { printTodos }
+    </div>
+  );
 }
